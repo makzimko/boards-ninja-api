af0a1ed0e11cc93b8fa48f1080d7378cb49f5e9a
Maksym Tkachuk <uakorn@gmail.com>
2021-08-06 18:46:18 +0300

    Fix dependencies

--- src/routes/system.ts
+++ src/routes/system.ts
@@ -3,7 +3,6 @@ import Router from '@koa/router';
 const systemRouter = new Router();
 
 systemRouter.get('/status', ctx => {
-  console.log('status');
   ctx.body = {
     uptime: process.uptime(),
   };
