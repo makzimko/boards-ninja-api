2827cc607ea272713b2b8debedd0c77d8bad19a5
Maksym Tkachuk <uakorn@gmail.com>
2021-08-07 17:21:06 +0300

    Added auto increment for WorkItems

--- src/models/WorkItem.ts
+++ src/models/WorkItem.ts
@@ -1,7 +1,17 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, connection } from 'mongoose';
+import autoIncrement from 'mongoose-auto-increment';
+
+autoIncrement.initialize(connection);
 
 const schema = new Schema({
+  id: 'number',
   name: 'string',
+});
+
+schema.plugin(autoIncrement.plugin, {
+  model: 'WorkItem',
+  field: 'id',
+  startAt: 1,
 });
 
 const WorkItemModel = model('WorkItem', schema);
